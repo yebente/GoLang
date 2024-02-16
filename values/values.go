c652f642c3dff8a56029535de09621db2e504def
yebente <yebente@gmail.com>
2024-02-16 17:40:40 +0300

    integers and floats

--- values/values.go
+++ values/values.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	//strings which can be added together with +
 	fmt.Println("go" + "lang")
+
+	//integers and floats
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("0.7/3.0 =", 0.7/0.3)
 }
