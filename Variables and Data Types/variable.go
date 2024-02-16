7db5f186b9cc8fcced30aae9b92558aeaf2b7b2c
yebente <yebente@gmail.com>
2024-02-16 16:57:57 +0300

    Variables and strings in different ways

--- Variables and Data Types/variable.go
+++ Variables and Data Types/variable.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	var musician string = "My favorite musician is Stromae"
+	var music = "His best song is Papa"
+	language := "His language of choice is French" //Can't be used outside of a func
 
 	fmt.Println(musician)
+	fmt.Println(music)
+	fmt.Println(language)
 }
