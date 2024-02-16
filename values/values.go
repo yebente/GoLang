b7a9b090f717ca4cdf7efa99f9f92ab4dd9d62c2
yebente <yebente@gmail.com>
2024-02-16 18:05:12 +0300

    Boolean Operators

--- values/values.go
+++ values/values.go
@@ -9,4 +9,19 @@ func main() {
 	//integers and floats
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("0.7/3.0 =", 0.7/0.3)
+
+	//Booleans, with boolean operators
+	fmt.Println(true && false)
+	fmt.Println(true && true)
+	fmt.Println(false && true)
+	fmt.Println(false && false)
+
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(false || true)
+	fmt.Println(false || false)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
+
 }
